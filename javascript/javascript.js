70fe4b11581644f0339ee531de83e0b1ccd7f74f
Rebeca Kethelyn Lopes <104172440+RebecaKLopes@users.noreply.github.com>
2023-01-29 01:18:43 -0300

    criei html da home

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,4 @@
+//função de login, para encaminhar até a pagina home
 function login(){
     var login = document.getElementById('ilogin').value;
     var senha = document.getElementById('isenha').value;
@@ -8,4 +9,22 @@ function login(){
     } else{
         alert('Usuário ou senha incorretos')
     }
+};
+
+// ativa ou desativa a classe ao clique no hamburguer
+const btnMobile = document.getElementById('btn-mobile');
+function toggleMenu(event){
+    if(event.type === 'touchstart')event.preventDefault()
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+    const active= nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'fechar menu')
+    } else{
+        event.currentTarget.setAttribute('aria-label', 'abrir menu')
+    }
 }
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
